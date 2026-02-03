41cd02f84b7db0a015f1301c508265796a302e18
IbrahimF1 <ifaruquee1@gmail.com>
2026-02-02 23:17:34 -0500

    Created user input

--- project.ts
+++ project.ts
@@ -18,7 +18,7 @@ const userPrompt = promptSync()
 // Retrieve deposit amount from frontpage
 const deposit = (): number => {
     while (true) {
-            const depositAmount: string = userPrompt("Enter a deposit amount: ")
+        const depositAmount: string = userPrompt("Enter a deposit amount: ")
         const numberDepositAmount: number = Number.parseFloat(depositAmount)
 
         if (Number.isNaN(numberDepositAmount) || numberDepositAmount <= 0) {
@@ -29,4 +29,36 @@ const deposit = (): number => {
     }
 }
 
-deposit()
+
+// Determine number of lines to bet on
+const getNumberOfLines = (): number => {
+    while (true) {
+        const lines: string = userPrompt("Enter the number of lines to bet on (1-3): ")
+        const numberOfLines: number = Number.parseFloat(lines)
+
+        if (Number.isNaN(numberOfLines) || numberOfLines <= 0 || numberOfLines > 3) {
+            console.log("❌ Invalid number of lines, try again.")
+        } else {
+            return numberOfLines
+        }
+    }
+}
+
+// Collect a bet amount
+const getBet = (balance: number, lines: number): number => {
+    while (true) {
+        const bet: string = userPrompt("Enter the bet per line: ")
+        const numberBet: number = Number.parseFloat(bet)
+
+        if (Number.isNaN(numberBet) || numberBet <= 0 || numberBet > balance / lines) {
+            console.log("❌ Invalid bet amount, try again.")
+        } else {
+            return numberBet
+        }
+    }
+}
+
+let balance = deposit()
+const numberOfLines = getNumberOfLines()
+
+const bet = getBet(balance, numberOfLines)
